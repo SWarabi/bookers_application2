75e776702f0b66057b5221a06d1c5b02e5e528e3
EC2 Default User <ec2-user@ip-172-31-20-112.ap-northeast-1.compute.internal>
2022-08-04 08:21:41 +0000

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,21 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  #追記
+  #追記8/2
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
   
+  #追記8/3
+  has_many :user_rooms, dependent: :destroy
+  has_many :rooms, through: :user_rooms
+  #追記8/4
+  has_many :chats, dependent: :destroy
+  #追記8/4
+  has_many :user_rooms, dependent: :destroy
+  has_many :view_counts, dependent: :destroy
   
   has_one_attached :profile_image
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'chats/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   get "search" => "searches#search"
   get "home/about"=>"homes#about"#, as: 'about'
   root to: 'homes#top'
   
+  #get 'chat/:id', to: 'chats#show', as: 'chat'
+  #resources :chats, only: [:create]
+  resources :chats, only: [:show, :create]
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
