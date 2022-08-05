57e6585fc7c52507f569b1931a1b5a71d79fd6d7
EC2 Default User <ec2-user@ip-172-31-20-112.ap-northeast-1.compute.internal>
2022-08-05 04:42:21 +0000

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :books
+   
+  has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   #追記8/2
